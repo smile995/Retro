5866a434641023eef124135902759290fd28e862
smile995 <ah.smile995@gmai.com>
2024-03-04 23:01:26 +0600

    this project is almost complete

--- javaScript.js
+++ javaScript.js
@@ -7,7 +7,7 @@ const apiData = async () => {
     arrayOfData.forEach(post => {
         const createDiv = document.createElement('div');
         createDiv.innerHTML = `
-        <div class="bg-slate-200 rounded-xl shadow-xl p-5 flex justify-between border-2 border-blue-400 ">
+        <div class="bg-slate-200 hover:bg-blue-100 rounded-xl shadow-xl p-5 flex justify-between border-2 border-blue-300 ">
         <div>
 
             <div>
@@ -45,7 +45,7 @@ const apiData = async () => {
                     </div>
                 </div>
                 <div class="rounded-full bg-green-600 px-3 py-2">
-                    <button id="bookMark" class="text-white"> <img src="./Icon/mark.png" alt="">
+                    <button onclick='btnFunction()' class="bookMark text-white"> <img src="./Icon/mark.png" alt="">
                     </button>
                 </div>
 
@@ -54,9 +54,99 @@ const apiData = async () => {
 
     </div>
         `
+
         postContainer.appendChild(createDiv)
     });
+
+
+
 }
+let count = 0
+const btnFunction = () => {
+
+    const counter = document.getElementById('count').innerHTML;
+    const digit = parseInt(count)
+    count = count + 1
+    const setValue = document.getElementById('count');
+    setValue.innerText = count;
+    console.log(digit);
+
+
+}
+
+const searchBtn = document.getElementById('search')
+
+searchBtn.addEventListener('click', () => {
+    const inputCategory = document.getElementById('category');
+    const category = inputCategory.value;
+    loadCategory(category)
+
+})
+
+const loadCategory = async (category) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`);
+    const allData = await response.json();
+    const posts = allData.posts;
+    const categoryContainer = document.getElementById('PostContainer');
+    categoryContainer.innerHTML='';
+    posts.forEach(post => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        
+        <div class="bg-slate-200 hover:bg-blue-100 rounded-xl shadow-xl p-5 flex justify-between border-2 border-blue-300 ">
+        <div>
+
+            <div>
+                <img class="w-[65px] rounded-xl" src="${post.image}" alt="">
+            </div>
+
+        </div>
+        <div class="flex-grow  ">
+            <div class="p-2 border-b-2 border-dashed space-y-2">
+                <div class="font-semibold flex items-center gap-3">
+                    <p># ${post.category}</p>
+                    <p>Author: ${post.author.name}</p>
+                </div>
+                <div>
+                    <h1 class="text-xl font-semibold">${post.title}</h1>
+                </div>
+                <div>
+                    <p>${post.description}</p>
+                </div>
+
+            </div>
+            <div class="p-2 flex  justify-between ">
+                <div class="flex items-center justify-between w-1/2 ">
+                    <div class="flex gap-2 items-center">
+                        <img src="./Icon/chat.png" alt="">
+                        <p>${post.comment_count}</p>
+                    </div>
+                    <div class="flex gap-2 items-center">
+                        <img src="./Icon/eye.png" alt="">
+                        <p>${post.view_count}</p>
+                    </div>
+                    <div class="flex gap-2 items-center">
+                        <img src="./Icon/back-in-time.png" alt="">
+                        <p>${post.posted_time}</p>
+                    </div>
+                </div>
+                <div class="rounded-full bg-green-600 px-3 py-2">
+                    <button onclick='btnFunction()' class="bookMark text-white"> <img src="./Icon/mark.png" alt="">
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+        
+        `
+
+        categoryContainer.appendChild(div)
+
+    })
+}
+
 
 const latestApi = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
@@ -90,6 +180,7 @@ const latestApi = async () => {
         </div>
     </div>
 `
+
         latestPostContainer.appendChild(div)
     })
 
